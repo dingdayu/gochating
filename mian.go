b23539819de4a7fdfa245c1fd058d1193abf6c1d
dingdayu <614422099@qq.com>
2017-03-04 20:00:46 +0800

    * 独立模板文件

--- mian.go
+++ mian.go
@@ -9,20 +9,12 @@ func main() {
 	// 注册一个路由
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		// 将字符串通过回写指针返回给浏览器
-		templates := `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-hello {{.name}}!
-</body>
-</html>
-		`
 		locals := make(map[string]interface{})
-		locals["locals"] = "dingdayu"
-		t := template.Must(template.New("templates").Parse(templates))
+		locals["name"] = "dingdayu"
+		t,err := template.ParseFiles("./templates/index.html")
+		if err != nil {
+			panic(err)
+		}
 		t.Execute(w, locals)
 	})
 	// 监听端口 8080
