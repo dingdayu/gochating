0fd31ac300467db3397b998b4cac77b249c290db
dingdayu <614422099@qq.com>
2017-03-04 19:31:57 +0800

    * 返回html（多行文本返回）

--- mian.go
+++ mian.go
@@ -9,7 +9,18 @@ func main() {
 	// 注册一个路由
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		// 将字符串通过回写指针返回给浏览器
-		io.WriteString(w, "hello word!")
+		template := `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+hello word!
+</body>
+</html>
+		`
+		io.WriteString(w, template)
 	})
 	// 监听端口 8080
 	err := http.ListenAndServe(":8080", nil)
