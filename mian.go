f2146106501bf72a39a12df3707aecb4440a635c
dingdayu <614422099@qq.com>
2017-03-04 19:46:39 +0800

    * 模板变量渲染
    Signed-off-by: dingdayu <614422099@qq.com>

--- mian.go
+++ mian.go
@@ -1,26 +1,28 @@
 package main
 
 import (
-	"io"
 	"net/http"
+	"text/template"
 )
 
 func main() {
 	// 注册一个路由
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		// 将字符串通过回写指针返回给浏览器
-		template := `<!DOCTYPE html>
+		templates := `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
 </head>
 <body>
-hello word!
+hello {{.}}!
 </body>
 </html>
 		`
-		io.WriteString(w, template)
+		name := "dingdayu"
+		t := template.Must(template.New("templates").Parse(templates))
+		t.Execute(w, name)
 	})
 	// 监听端口 8080
 	err := http.ListenAndServe(":8080", nil)
