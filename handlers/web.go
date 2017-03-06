80828815fcf7222eeedd6a0b55063d1412e33c95
dingdayu <614422099@qq.com>
2017-03-07 00:35:41 +0800

    * 利用闭包规避运行时错误崩溃
    Signed-off-by: dingdayu <614422099@qq.com>

--- handlers/web.go
+++ handlers/web.go
@@ -8,12 +8,22 @@ import (
 
 func PublicHandler(w http.ResponseWriter, r *http.Request) {
 	// 直接调用http包提供的文件服务方法，直接根据请求路径返回文件内容
-	if exists := os.IsExist(r.URL.Path[len("/"):]); !exists {
+	name := r.URL.Path[len("/"):];
+	if exists := isExists(name); !exists {
 		// 文件找不到返回404
 		http.NotFound(w, r)
 		return
 	}
-	http.ServeFile(w, r, r.URL.Path[len("/"):])
+	http.ServeFile(w, r, name)
+}
+
+// 判断对应文件是否存在
+func isExists(path string) bool  {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	return os.IsExist(err)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request)  {
--- mian.go
+++ mian.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	// 注册一个路由
-	http.HandleFunc("/hello", handlers.Hello)
+	http.HandleFunc("/hello", safeWebHandler(handlers.Hello))
 	http.HandleFunc("/api/json", handlers.HelloJson)
 	http.HandleFunc("/public/", handlers.PublicHandler)
 
@@ -15,5 +15,28 @@ func main() {
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
+	}
+}
+
+// 服务器内部错误拦截
+func safeWebHandler(fn http.HandlerFunc) http.HandlerFunc  {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// 遇到错误时的扫尾工作
+		defer func() {
+			// 终止（拦截）错误的传递
+			if e, ok := recover().(error); ok {
+				http.Error(w, e.Error(), http.StatusInternalServerError)
+				// 或者输出自定义的50x错误页面
+				//w.WriteHeader(http.StatusInternalServerError)
+				//t,err := template.ParseFiles("./templates/50x.html")
+				//if err != nil {
+				//	// 抛出错误，并向上层层抛出错误
+				//	panic(err)
+				//}
+				//t.Execute(w, nil)
+			}
+		}()
+		// 调用传入的方法名
+		fn(w, r)
 	}
 }
