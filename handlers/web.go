6b1e17871aece94c164a5e6f297589f5ee478f13
dingdayu <614422099@qq.com>
2017-03-07 00:16:25 +0800

    * 静态资源文件

--- handlers/web.go
+++ handlers/web.go
@@ -3,7 +3,18 @@ package handlers
 import (
 	"net/http"
 	"html/template"
+	"os"
 )
+
+func PublicHandler(w http.ResponseWriter, r *http.Request) {
+	// 直接调用http包提供的文件服务方法，直接根据请求路径返回文件内容
+	if exists := os.IsExist(r.URL.Path[len("/"):]); !exists {
+		// 文件找不到返回404
+		http.NotFound(w, r)
+		return
+	}
+	http.ServeFile(w, r, r.URL.Path[len("/"):])
+}
 
 func Hello(w http.ResponseWriter, r *http.Request)  {
 	// 将字符串通过回写指针返回给浏览器
--- mian.go
+++ mian.go
@@ -9,6 +9,7 @@ func main() {
 	// 注册一个路由
 	http.HandleFunc("/hello", handlers.Hello)
 	http.HandleFunc("/api/json", handlers.HelloJson)
+	http.HandleFunc("/public/", handlers.PublicHandler)
 
 	// 监听端口 8080
 	err := http.ListenAndServe(":8080", nil)
